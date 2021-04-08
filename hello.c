d3386bd320f9ed4ec00065479a8484152efd7c2e
Zoe Yang <yb_tju@126.com>
2021-04-08 14:47:41 +0800

    Added a new hello world c program!

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * @,@Author: ,: your name
+ * @,@Date: ,: 2021-04-08 14:43:47
+ * @,@LastEditTime: ,: 2021-04-08 14:45:00
+ * @,@LastEditors: ,: Please set LastEditors
+ * @,@Description: ,: In User Settings Edit
+ * @,@FilePath: ,: /undefined/home/zoe/git_demo/MyFirstProject/hello.c
+ */
+# include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+}
